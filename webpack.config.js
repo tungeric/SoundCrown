d96fe4e5a069fe66fda0df95d010d00f954c5670
Eric Tung <eric.p.tung@gmail.com>
2017-09-29 16:15:14 -0700

    added uglify

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,11 @@ module.exports = {
           'process.env':{
               'NODE_ENV': JSON.stringify('production')
           }
-      })
+      }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
   ]
 };
