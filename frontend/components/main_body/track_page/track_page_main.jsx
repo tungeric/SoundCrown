acef93ada87a61b1f3b57228b82d279864cff639
Eric Tung <eric.p.tung@gmail.com>
2017-09-30 08:52:34 -0700

    fixed issue where track pages won't show music animation

--- frontend/components/main_body/track_page/track_page_main.jsx
+++ frontend/components/main_body/track_page/track_page_main.jsx
@@ -22,20 +22,21 @@ class TrackPageMain extends React.Component {
   }
 
   componentWillReceiveProps(nextProps){
-    if(this.state.track){
-      if((nextProps.track.id === this.state.track.id)) {
-        if (this.state.play !== nextProps.trackData.play) {
-          this.props.getTrack(nextProps.track.id);
-          this.setState({
-            play: nextProps.trackData.play
-          });
-        }
-      }
+    if (nextProps.trackData.active &&
+        this.props.track.audio_url === nextProps.trackData.active.url &&
+        this.state.play !== nextProps.trackData.play){
+      this.setState({ play: nextProps.trackData.play});
+    }
+
+    if (nextProps.trackData.active &&
+        this.props.track.audio_url !== nextProps.trackData.active.url &&
+        this.state.play === nextProps.trackData.play){
+      this.setState({ play: false});
     }
   }
 
   renderElapsedTime() {
-    let seconds = Math.floor((new Date() - this.state.created_at) / 1000);
+    let seconds = Math.floor((new Date() - this.props.track.created_at) / 1000);
     let interval = Math.floor(seconds / 31536000);
     if (interval > 0) {
       return interval + " years";
@@ -84,10 +85,10 @@ class TrackPageMain extends React.Component {
   }
 
   render () {
-    const active = this.state.track;
-    const play = this.state.play;
-    if(this.state.title) {
-      if (this.state.title.length > 0) {
+    const active = this.props.track;
+    const play = this.state.play && (this.state.id === this.props.track.id);
+    if(this.props.track) {
+      if (this.props.track.title.length > 0) {
         let playPauseClass = classnames('fa', {'fa-pause': play}, {'fa-play': !play});
         let animationPlayPauseClass = classnames({'now playing': play}, {'paused' : !play });
         return (
@@ -99,8 +100,8 @@ class TrackPageMain extends React.Component {
                     <i className={playPauseClass} />
                   </button>
                   <div>
-                    <Link className="track-header-username" to={`/${this.state.creator}/`}>{this.state.creator}</Link>
-                    <div className="track-header-trackname">{this.state.title}</div>
+                    <Link className="track-header-username" to={`/${this.props.track.creator}/`}>{this.props.track.creator}</Link>
+                    <div className="track-header-trackname">{this.props.track.title}</div>
                     <div className="more-options">
                       {this.renderTrackMenu()}
                     </div>
@@ -111,7 +112,7 @@ class TrackPageMain extends React.Component {
                 <br/><br/><br/><br/><br/>
                 <div className="track-playbar-and-options">
                   <div className="track-header-progress-container">
-                    <div className={animationPlayPauseClass} id={`music-animation-${this.state.id}`}>
+                    <div className={animationPlayPauseClass} id={`music-animation-${this.props.track.id}`}>
                       <span className="bar n1"></span>
                       <span className="bar n2"></span>
                       <span className="bar n3"></span>
@@ -168,13 +169,13 @@ class TrackPageMain extends React.Component {
                       <span className="bar n4"></span>
                     </div>
                     <br/>
-                    <div className="num-plays"><i className="fa fa-play"/> {this.state.plays} </div>
+                    <div className="num-plays"><i className="fa fa-play"/> {this.props.track.plays} </div>
                   </div>
                 </div>
               </div>
               <div className="track-header-right">
                 <div className="track-cover-art-container">
-                  <img className="track-cover-art" src={this.state.cover_art_url}/>
+                  <img className="track-cover-art" src={this.props.track.cover_art_url}/>
                 </div>
               </div>
             </div>
