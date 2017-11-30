9ef8c66eb89ddd94c8dcfd8986ea393daaa82e7b
Eric Tung <eric.p.tung@gmail.com>
2017-11-30 14:07:08 -0800

    Redo tag model

--- db/migrate/20171130220649_create_tags.rb
+++ db/migrate/20171130220649_create_tags.rb
@@ -1,10 +1,8 @@
 class CreateTags < ActiveRecord::Migration[5.1]
   def change
     create_table :tags do |t|
-      t.string :name, null: false
-      t.integer :track_id, null: false
+
       t.timestamps
     end
-    add_index :tags, :track_id
   end
 end
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,14 +1,3 @@
-# == Schema Information
-#
-# Table name: tags
-#
-#  id         :integer          not null, primary key
-#  name       :string           not null
-#  track_id   :integer          not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
