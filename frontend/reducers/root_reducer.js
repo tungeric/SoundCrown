2dbff09cd4090532cd7c81dd18bc4a83ec2e8136
Eric Tung <eric.p.tung@gmail.com>
2017-12-01 01:36:15 -0800

    Add tags to root and error reducers

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -4,11 +4,13 @@ import TracksReducer from './tracks_reducer';
 import UsersReducer from './users_reducer';
 import ErrorsReducer from './errors_reducer';
 import CommentsReducer from './comments_reducer';
+import TagsReducer from './tags_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
   tracks: TracksReducer,
   users: UsersReducer,
+  tags: TagsReducer,
   errors: ErrorsReducer,
   comments: CommentsReducer
 });
